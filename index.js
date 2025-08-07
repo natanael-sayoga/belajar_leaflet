d0770a82e61dba4d638f2ef7865b904c1b9a7c47
natanael-sayoga <nsayoga@gmail.com>
2025-08-08 00:10:00 +0700

    commit 2.5: playing with marker and simple event handler

--- index.js
+++ index.js
@@ -23,11 +23,68 @@ in return we will get an image data to be shown as a map to the user!
 
 let myMap = L.map('myMap', {
     center: [-7.5407, 110.4457],
-    zoom: 14
-});
+    zoom: 12
+})
 let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 })
-osm.addTo(myMap);
 
+let myMarker = {
+    merapi:{
+        coordinate:[-7.5407, 110.4457],
+        tooltipMessage:"Mt. Merapi Peak",
+        popupContent:`
+            located at:<br>
+            <b>(-7.5407° S, 110.4457° E)</b>
+            `
+    },
+    ngandongPineForest:{
+        coordinate:[-7.5852, 110.4128],
+        tooltipMessage:"Ngandong Pine Forrest",
+        popupContent:`
+        located at:<br>
+        <b>(-7.5852° S, 110.4128° E)</b>
+        `
+    },
+    draggableMarker:{
+        coordinate:[-7.5816, 110.4474],
+        tooltipMessage:"YOU ARE HERE!",
+        icon:L.icon({
+            iconUrl: 'img/people.png',
+            iconSize: [40, 40],
+            // iconAnchor: [22, 94],
+            // popupAnchor: [-3, -76],
+            // shadowUrl: 'my-icon-shadow.png',
+            // shadowSize: [68, 95],
+            // shadowAnchor: [22, 94]
+        })
+    }
+}
+let merapiMarker = L.marker(myMarker.merapi.coordinate)
+merapiMarker.bindTooltip(myMarker.merapi.tooltipMessage).openTooltip();
+merapiMarker.bindPopup(myMarker.merapi.popupContent).openPopup()
+
+let ngandongMarker = L.marker(myMarker.ngandongPineForest.coordinate)
+ngandongMarker.bindTooltip(myMarker.ngandongPineForest.tooltipMessage)
+ngandongMarker.bindPopup(myMarker.ngandongPineForest.popupContent)
+
+let draggableMarker = L.marker(myMarker.draggableMarker.coordinate, {
+    icon:myMarker.draggableMarker.icon,
+    draggable:true,
+})
+draggableMarker.bindTooltip(myMarker.draggableMarker.tooltipMessage)
+https://leafletjs.com/reference.html#marker
+//https://leafletjs.com/reference.html#event
+draggableMarker.on("move", function(e){
+    myMarker.draggableMarker.tooltipMessage="we are moving"
+    //draggableMarker.bindTooltip(myMarker.draggableMarker.tooltipMessage)
+    // myMarker.draggableMarker.tooltipMessage=`You are now located at:<br>
+    // <b>(${draggableMarker.getLatLng().lat}°, ${draggableMarker.getLatLng().lng}°)</b>`
+})
+
+
+osm.addTo(myMap);
+merapiMarker.addTo(myMap)
+ngandongMarker.addTo(myMap)
+draggableMarker.addTo(myMap)
